47cdcc77c4fc7d1126a078add13521e095b7d9f7
ermias-vm <ermiasvalls.m@gmail.com>
2025-04-09 21:31:32 +0200

    Fix cyclope

--- Whomp’em/src/Cyclope.cpp
+++ Whomp’em/src/Cyclope.cpp
@@ -5,59 +5,46 @@
 #include "Scene.h"
 #include "Cyclope.h"
 
-#define JUMP_ANGLE_STEP 4
-#define JUMP_HEIGHT 64.0f
-#define JUMP_WIDTH 96.0f
-#define PI 3.14159265
-
-#define FLY_ANIM_SPEED 50
-#define STAND_ANIM_SPEED 100
-#define JUMP_ANIM_SPEED 50
-
-#define JUMP_WIDTH 96.0f
+#define JUMP_HEIGHT 60
+#define JUMP_WIDTH_DEF 70
+#define JUMP_DURATION 0.8f
+#define GROUND_DELAY 1.0f
+#define TRIGGER_DISTANCE 200
+#define CLOSE_DISTANCE 50  // Distancia para salto exacto
 
 enum CyclopeAnims {
     STAND_RIGHT, STAND_LEFT, JUMP_RIGHT, JUMP_LEFT, FLY_RIGHT, FLY_LEFT
 };
 
-
-
-
 void Cyclope::init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram) {
     tileMapDispl = tileMapPos;
-	isLookingLeft = isJumping = false;  
-    // Cargar spritesheet
+    isJumping = false;
+    bLookingLeft = false;
+    timeOnGround = 0.0f;
+	jumpWidth = JUMP_WIDTH_DEF;
     cyclopeSpritesheet.loadFromFile("images/sprites/cyclopeFrames.png", TEXTURE_PIXEL_FORMAT_RGBA);
-
-    // Crear sprite
     cyclopeSprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25, 0.25), &cyclopeSpritesheet, &shaderProgram);
     cyclopeSprite->setNumberAnimations(6);
 
-    // Animación STAND_RIGHT
-    cyclopeSprite->setAnimationSpeed(STAND_RIGHT, STAND_ANIM_SPEED);
+    cyclopeSprite->setAnimationSpeed(STAND_RIGHT, 100);
     cyclopeSprite->addKeyframe(STAND_RIGHT, glm::vec2(0.0f, 0.0f));
 
-    // Animación STAND_LEFT
-    cyclopeSprite->setAnimationSpeed(STAND_LEFT, STAND_ANIM_SPEED);
+    cyclopeSprite->setAnimationSpeed(STAND_LEFT, 100);
     cyclopeSprite->addKeyframe(STAND_LEFT, glm::vec2(0.25f, 0.0f));
 
-    // Animación JUMP_RIGHT
-    cyclopeSprite->setAnimationSpeed(JUMP_RIGHT, JUMP_ANIM_SPEED);
+    cyclopeSprite->setAnimationSpeed(JUMP_RIGHT, 50);
     cyclopeSprite->addKeyframe(JUMP_RIGHT, glm::vec2(0.0f, 0.25f));
 
-    // Animación JUMP_LEFT
-    cyclopeSprite->setAnimationSpeed(JUMP_LEFT, JUMP_ANIM_SPEED);
+    cyclopeSprite->setAnimationSpeed(JUMP_LEFT, 50);
     cyclopeSprite->addKeyframe(JUMP_LEFT, glm::vec2(0.25f, 0.25f));
 
-    // Animación FLY_RIGHT
-    cyclopeSprite->setAnimationSpeed(FLY_RIGHT, FLY_ANIM_SPEED);
+    cyclopeSprite->setAnimationSpeed(FLY_RIGHT, 50);
     cyclopeSprite->addKeyframe(FLY_RIGHT, glm::vec2(0.0f, 0.5f));
     cyclopeSprite->addKeyframe(FLY_RIGHT, glm::vec2(0.25f, 0.5f));
     cyclopeSprite->addKeyframe(FLY_RIGHT, glm::vec2(0.50f, 0.5f));
     cyclopeSprite->addKeyframe(FLY_RIGHT, glm::vec2(0.25f, 0.5f));
 
-    // Animación FLY_LEFT
-    cyclopeSprite->setAnimationSpeed(FLY_LEFT, FLY_ANIM_SPEED);
+    cyclopeSprite->setAnimationSpeed(FLY_LEFT, 50);
     cyclopeSprite->addKeyframe(FLY_LEFT, glm::vec2(0.0f, 0.75f));
     cyclopeSprite->addKeyframe(FLY_LEFT, glm::vec2(0.25f, 0.75f));
     cyclopeSprite->addKeyframe(FLY_LEFT, glm::vec2(0.50f, 0.75f));
@@ -66,71 +53,110 @@ void Cyclope::init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram) {
     cyclopeSprite->changeAnimation(STAND_RIGHT);
 }
 
+void Cyclope::changeAnimToRightLeft(int animation) {
+    if (bLookingLeft) {
+        animation++;
+    }
+    cyclopeSprite->changeAnimation(animation);
+}
+
 void Cyclope::update(int deltaTime) {
     cyclopeSprite->update(deltaTime);
+    float dt = deltaTime / 1000.0f;
 
-    if (!isJumping) {
-        // Calcular la posición del jugador
-        glm::vec2 playerPos = Game::instance().getScene()->getPlayerPos();
-        targetPos = playerPos;
-
-        // Iniciar un salto hacia el jugador
-        startJump();
-    }
-    else {
-        // Actualizar el salto
-        jumpAngle += JUMP_ANGLE_STEP;
-
-        if (jumpAngle >= 180) {
-            // El salto ha terminado
-            isJumping = false;
-            posCyclope.y = startY; // Asegurarse de que el Cyclope esté en el suelo
-            cyclopeSprite->changeAnimation(STAND_RIGHT); // Volver a la animación de estar parado
+    if (bLookingLeft) {
+        if (map->collisionMoveLeft(posCyclope, glm::ivec2(25, 32))) {
+            posCyclope.x += 1;
         }
-        else {
-            // Calcular la posición intermedia durante el salto
-            float directionX = (targetPos.x - posCyclope.x) > 0 ? 1.0f : -1.0f;
-            posCyclope.x = jumpStartPos.x + (JUMP_WIDTH * sin(PI * jumpAngle / 180.f) * directionX);
-            posCyclope.y = jumpStartPos.y - (JUMP_HEIGHT * sin(PI * jumpAngle / 180.f));
+    }
 
-            // Cambiar a la animación de vuelo después de unos milisegundos
-            if (jumpAngle > 10) {
-                cyclopeSprite->changeAnimation(directionX > 0 ? FLY_RIGHT : FLY_LEFT);
+	else {
+		if (map->collisionMoveRight(posCyclope, glm::ivec2(28, 32))) {
+			posCyclope.x -= 1;
+		}
+	}
+
+    if (isJumping) {
+        jumpTime += dt;
+        float t = jumpTime / JUMP_DURATION;
+        posCyclope.x = jumpStartPos.x + static_cast<int>(jumpWidth * t * jumpDirection);
+        posCyclope.y = jumpStartPos.y - static_cast<int>(jumpWidth * sinf(3.14159f * t));
+
+        if (map->collisionMoveLeft(posCyclope, glm::ivec2(25, 32))) {
+            posCyclope.x += 1;
+        }
+        if (map->collisionMoveRight(posCyclope, glm::ivec2(28, 32))) {
+            posCyclope.x -= 1;
+        }
+
+        if (jumpTime >= JUMP_DURATION || map->collisionMoveDown(posCyclope, glm::ivec2(32, 32), &posCyclope.y)) {
+            isJumping = false;
+            timeOnGround = 0.0f;
+            if (cyclopeSprite->animation() != (STAND_RIGHT + bLookingLeft)) {
+                changeAnimToRightLeft(STAND_RIGHT);
             }
         }
-
-        // Actualizar la posición del sprite
-        cyclopeSprite->setPosition(glm::vec2(
-            float(tileMapDispl.x + posCyclope.x),
-            float(tileMapDispl.y + posCyclope.y)
-        ));
+        else {
+            if (cyclopeSprite->animation() != (FLY_RIGHT + bLookingLeft)) {
+                changeAnimToRightLeft(FLY_RIGHT);
+            }
+        }
     }
+    else {
+        if (map->collisionMoveDown(posCyclope, glm::ivec2(32, 32), &posCyclope.y)) {
+            timeOnGround += dt;
+            if (timeOnGround >= GROUND_DELAY) {
+                glm::vec2 playerPosFloat = Game::instance().getScene()->getPlayerPos();
+                glm::ivec2 playerPos(static_cast<int>(playerPosFloat.x), static_cast<int>(playerPosFloat.y));
+                int distanceX = abs(playerPos.x - posCyclope.x);
+
+                if (distanceX < TRIGGER_DISTANCE) {
+                    jumpStartPos = posCyclope;
+                    jumpTime = 0.0f;
+                    isJumping = true;
+                    jumpDirection = (playerPos.x > posCyclope.x) ? 1 : -1;
+                    bLookingLeft = (jumpDirection < 0);
+                    if  (distanceX < CLOSE_DISTANCE){
+                        if (playerPos.y != posCyclope.y) jumpWidth = JUMP_WIDTH_DEF - 20;
+						else jumpWidth = distanceX;
+					}
+					else jumpWidth = JUMP_WIDTH_DEF;
+						
+                    if (cyclopeSprite->animation() != (JUMP_RIGHT + bLookingLeft)) {
+                        changeAnimToRightLeft(JUMP_RIGHT);
+                    }
+                }
+            }
+            if (cyclopeSprite->animation() != (STAND_RIGHT + bLookingLeft)) {
+                changeAnimToRightLeft(STAND_RIGHT);
+            }
+        }
+        else {
+            posCyclope.y += 4;
+
+            if (cyclopeSprite->animation() != (FLY_RIGHT + bLookingLeft)) {
+                changeAnimToRightLeft(FLY_RIGHT);
+            }
+        }
+    }
+
+    cyclopeSprite->setPosition(glm::vec2(
+        float(tileMapDispl.x + posCyclope.x),
+        float(tileMapDispl.y + posCyclope.y)
+    ));
 }
 
 void Cyclope::render() {
     cyclopeSprite->render();
 }
 
-void Cyclope::setPosition(const glm::vec2& pos) {
+void Cyclope::setPosition(const glm::ivec2& pos) {
     posCyclope = pos;
     cyclopeSprite->setPosition(glm::vec2(float(tileMapDispl.x + posCyclope.x), float(tileMapDispl.y + posCyclope.y)));
 }
 
-glm::vec2 Cyclope::getPosition() const {
+glm::ivec2 Cyclope::getPosition() const {
     return posCyclope;
-}
-
-void Cyclope::startJump() {
-    isJumping = true;
-    jumpAngle = 0;
-    jumpStartPos = posCyclope;
-    startY = posCyclope.y;
-
-    // Determinar la dirección del salto
-    float directionX = (targetPos.x - posCyclope.x) > 0 ? 1.0f : -1.0f;
-
-    // Cambiar a la animación de salto
-    cyclopeSprite->changeAnimation(directionX > 0 ? JUMP_RIGHT : JUMP_LEFT);
 }
 
 void Cyclope::setTileMap(TileMap* tileMap) {
